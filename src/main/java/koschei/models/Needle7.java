f83755fc8ddaf11b6bfd2dc946cb4e5ea0e9337e
Vlad <buchik980@gmail.com>
2024-01-05 14:36:49 +0300

    task completed

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,19 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    private final Death8 death;
+
+    public Needle7(Death8 death) {
+        this.death = death;
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + death.toString();
+    }
+
+}
